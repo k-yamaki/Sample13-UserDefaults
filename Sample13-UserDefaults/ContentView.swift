81403a8fd46be53947b37cc5939c319c05d40818
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2021-01-22 18:28:05 +0900

    Initial Commit

---
+++ Sample13-UserDefaults/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample13-UserDefaults
+//
+//  Created by keiji yamaki on 2021/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample13-UserDefaults/Sample13_UserDefaultsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sample13_UserDefaultsApp.swift
+//  Sample13-UserDefaults
+//
+//  Created by keiji yamaki on 2021/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Sample13_UserDefaultsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
